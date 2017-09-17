a6f5bcd8e4d1bfbb3f3074effd4656dc3a90d1c0
Treven Trujillo <treventrujillo@gmail.com>
2017-09-17 12:34:01 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react;'
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <Container>
+        
+        </Container>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/MovieList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+
+
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default MovieList;
